fda55b22640c4d0ba3af0590ccba75baf8da70a6
AlexanderSolovyev <1430@list.ru>
2018-09-04 12:30:26 +0300

    get_list send id user to 1c

--- app/controllers/goods_controller.rb
+++ app/controllers/goods_controller.rb
@@ -1,7 +1,7 @@
 require "net/http"
 require "uri"
 class GoodsController < ApplicationController
-  skip_before_action :authenticate_request
+  skip_before_action :authenticate_request, only: :image
   def list
     a=get_list
     a.slice!("catalog=")
@@ -22,9 +22,12 @@ class GoodsController < ApplicationController
 
   def get_list
     #uri = URI.parse("http://app.heavesi.ee:8000/hv_copy/hs/mobile/GetCatalog")
-
+    id = {
+      'ID' => @current_user.id
+    }
     uri = URI.parse("http://192.168.4.201/hv_copy/hs/mobile/GetCatalog")
-    req = Net::HTTP::Get.new(uri)
+    req = Net::HTTP::Post.new(uri)
+    req.body = id.to_json
     req.basic_auth 'exch', '13572468'
     res = Net::HTTP.start(uri.hostname, uri.port){ |http| http.request(req) }
     res.body
