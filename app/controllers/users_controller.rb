f5f44c054cca7e6bfea04544d5a87fe34ac8efc4
Александр Соловьев <solovev@MacBook-Air-Aleksandr-2.local>
2018-11-08 17:13:52 +0300

    registration to website

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,14 +1,24 @@
+require 'net/http'
 class UsersController < ApplicationController
   skip_before_action :authenticate_request
 
   def registration
-    user=User.new(user_params_pass)
-    if user.save
-      SendUserJob.perform_later(user,@pass)
-      render json: { ok: 'registration completed'}, status: 200
+    user=User.new(user_params)
+    tok=token
+    res =reg user, tok
+    result = JSON.parse(res.body)
+    byebug
+    if (res.code == "201")
+      render json: {}, status: 200
     else
-      render json: {error: user.errors}, status: 500
+      render json: {error: result["message"]}, status: 500
     end
+    #if user.save
+    #  SendUserJob.perform_later(user,@pass)
+     #  render json: { ok: 'registration completed'}, status: 200
+    #else
+    #  render json: {error: user.errors}, status: 500
+    #end
   end
 
   #def resend
@@ -22,6 +32,42 @@ class UsersController < ApplicationController
 
   private
 
+  def token
+    uri = URI.parse("https://heavesi.ee/wp-json/jwt-auth/v1/token")
+    #                http://192.168.4.201/hv_copy/hs/mobile/GetConfirm
+    #uri = URI.parse("http://212.7.4.74:8000/hv_copy/hs/GetConfirm?")
+    info = {
+      'username' => 'mobileapp@heavesi.ee',
+      'password' => 'BjwYKyVJO&)$4e0Jls$W&Dj)'
+    }
+    req = Net::HTTP::Post.new(uri)
+    req.content_type = "application/json"
+    req.add_field("cache-control", "no-cache")
+    req.body = info.to_json
+    res = Net::HTTP.start(uri.hostname, uri.port, :use_ssl => uri.scheme == 'https'){ |http| http.request(req) }
+    result = JSON.parse(res.body)
+    result["token"]
+  end
+
+  def reg (user,token)
+    uri = URI.parse("https://heavesi.ee/wp-json/wc/v2/customers")
+    info = {
+      "username" => user.name,
+      "password" => user.password,
+      "first_name" => user.name,
+      "email" => user.email,
+      "billing" => { "phone" => user.phone},
+      "meta_data" => [{"key" => "reg_number", "value" => user.reg_number}]
+    }
+    req = Net::HTTP::Post.new(uri)
+    req.content_type = "application/json"
+    req.add_field("cache-control", "no-cache")
+    req.add_field("Authorization", "Bearer "+token)
+    req.body = info.to_json
+    res = Net::HTTP.start(uri.hostname, uri.port, :use_ssl => uri.scheme == 'https'){ |http| http.request(req) }
+
+  end
+
   #def user_params_pass
   #  u = user_params
   #  @pass=create_pass
@@ -29,7 +75,7 @@ class UsersController < ApplicationController
   #  return u
   #end
   def user_params
-    params.require(:user).permit(:name, :email, :phone, :reg_number)
+    params.require(:user).permit(:name, :email, :phone, :reg_number, :password)
   end
 
   #def create_pass
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180420122959) do
+ActiveRecord::Schema.define(version: 20181108121951) do
 
   create_table "goods", force: :cascade do |t|
     t.string "good_name"
@@ -54,6 +54,7 @@ ActiveRecord::Schema.define(version: 20180420122959) do
     t.string "name"
     t.string "phone"
     t.boolean "confirm", default: false
+    t.string "password"
   end
 
 end
